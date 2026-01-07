8b3258c990ffd7643e5c3f8817e0fcdb852da7e5
Matt Conroy <matt@provenance.io>
2026-01-07 16:14:00 -0500

    - GetNodeConfig... not using it, but may as well have it.

--- pkg/provenance/node.go
+++ pkg/provenance/node.go
@@ -13,3 +13,12 @@ func (c *ProvenanceClient) GetNodeInfo(ctx context.Context) (*nodetypes.StatusRe
 	}
 	return res, nil
 }
+
+// Get node config
+func (c *ProvenanceClient) GetNodeConfig(ctx context.Context) (*nodetypes.ConfigResponse, error) {
+	res, err := (*c.NodeClient()).Config(ctx, &nodetypes.ConfigRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
